48770f241e0db53961a27836e096b3faa933c763
Cheng Zhao <zcbenz@gmail.com>
2022-08-03 12:24:31 +0900

    Add build script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,17 +4,19 @@ set -e           # exit when command fails
 set -o pipefail  # exit when pipe fails
 set -x           # print running command
 
+# Download source tarball.
 CHROMIUM_URL=https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$VERSION.tar.xz
 curl -# $CHROMIUM_URL | tar Jxf -
+
+# Download clang.
+LLVM_URL=https://commondatastorage.googleapis.com/chromium-browser-clang/Linux_x64/clang-llvmorg-15-init-10168-gc2a7904a-1.tgz
+curl -# $LLVM_URL | tar zxf - --directory chromium-$VERSION/third_party/llvm-build/Release+Asserts
 
 mv chromium-$VERSION src
 cd src
 
-LLVM_URL=https://commondatastorage.googleapis.com/chromium-browser-clang/Linux_x64/clang-llvmorg-15-init-10168-gc2a7904a-1.tgz
-curl -# $LLVM_URL | tar zxf - --directory third_party/llvm-build/Release+Asserts
+# Download sysroot.
+./build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
 
-mkdir -p out/Release
-cat >> out/Release/args.gn <<-EOF
-  use_sysroot=false
-  is_official_build=true
-EOF
+# Download nodejs.
+./third_party/node/update_node_binaries
